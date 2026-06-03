678db7347419e97ae8a9de7f89aa2211a8de4f29
amanpandey05 <7266064524aman@gmail.com>
2026-06-03 23:44:18 +0530

    Update container_with_most_water.cpp

--- week1/day3/container_with_most_water.cpp
+++ week1/day3/container_with_most_water.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int i = 0;
+        int j = n-1;
+        int maxi = 0;
+        while(i<j) {
+            int h = min(height[i], height[j])*(j-i);
+            maxi = max(maxi, h);
+          if(height[i] < height[j]) {
+           i++; 
+          }
+           else {
+           j--;
+          }
+        }
+        return maxi;
+    }
+    
+};
