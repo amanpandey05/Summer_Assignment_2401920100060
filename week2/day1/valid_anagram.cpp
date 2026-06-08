dca4a9ca1ef45f5741bfacf45bfe8bd5ef784c72
amanpandey05 <7266064524aman@gmail.com>
2026-06-08 17:00:21 +0530

    Update valid_anagram.cpp

--- week2/day1/valid_anagram.cpp
+++ week2/day1/valid_anagram.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      sort(s.begin(), s.end());
+      sort(t.begin(), t.end());
 
+      int n = s.size();
+      int m = t.size();
+       int i = 0;
+       int j = 0;
+     if(n<m || m<n){
+        return false;
+     }
+      while(i < n && j < m) {
+        if(s[i] != t[j])  {
+            return false;
+           i++;
+           j++;
+        }else {
+            i++;
+            j++;
+        }
+        
+            
+        }
+      return true; 
+    }
+};
