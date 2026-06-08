8fb8b65b6c629fd516bc719bc6e639a9d22fdeaa
amanpandey05 <7266064524aman@gmail.com>
2026-06-08 23:13:12 +0530

    Update ransom-Note.cpp

--- week2/day1/ransom-Note.cpp
+++ week2/day1/ransom-Note.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+     unordered_map<char, int> mp;
+       for(auto c: magazine) {
+        mp[c]++;
+       } 
+       for(int i = 0; i<ransomNote.size(); i++) {
+        if(mp[ransomNote[i]] > 0) {
+           mp[ransomNote[i]]--;
+        } else {
+         return false;
+        }
+       }
+       return true;
+    }
+};
