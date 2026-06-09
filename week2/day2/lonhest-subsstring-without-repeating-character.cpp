907eb347b37628ca39f9e69068c2bcf7a42ee26f
amanpandey05 <7266064524aman@gmail.com>
2026-06-09 21:31:43 +0530

    Update lonhest-subsstring-without-repeating-character.cpp

--- week2/day2/lonhest-subsstring-without-repeating-character.cpp
+++ week2/day2/lonhest-subsstring-without-repeating-character.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        int l = 0, r = 0, maxlen = 0;
+      vector<int> hash(256, -1);
+        while(r < s.size()){
+            if(hash[s[r]]!= -1){
+                if(hash[s[r]] >= l){
+                    l = hash[s[r]]+1;
 
+                }
+            }
+            int len = r-l+1;
+        maxlen = max(len , maxlen);
+        hash[s[r]] = r;
+        r++;
+       
+        }
+         return maxlen;
+        
+    }
+};
