fb81be207089c88e9385f2a1bbe3afdb379ae387
amanpandey05 <7266064524aman@gmail.com>
2026-06-09 20:59:15 +0530

    Update find-all-anagram-int-a-string.cpp

--- week2/day2/find-all-anagram-int-a-string.cpp
+++ week2/day2/find-all-anagram-int-a-string.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+     vector<int> res;
+     if (p.size() > s.size()) return res;
+       vector<int> pf(26,0);
+       vector<int> wf(26, 0);
+       for(int i = 0; i<p.size(); i++) {
+        pf[p[i]-'a']++;
+        
+       }
+      
+      
+       for (int i = 0; i < p.size(); i++) {
+             wf[s[i] - 'a']++;
+        }
+         if(wf == pf) res.push_back(0);
+        int l = 0;
+        int r = p.size();
+        while(r<s.size()) {
+          wf[s[r]-'a']++;
+          wf[s[l]-'a']--;
+          l++;
+          r++;
+          if(wf == pf) res.push_back(l);
+        }
+        return res;
+    }
+};
