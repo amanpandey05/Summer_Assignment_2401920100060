af0f182f165849e10d79a88236a3b656bae01c0f
amanpandey05 <7266064524aman@gmail.com>
2026-06-11 21:29:18 +0530

    Update reverse-word-in-a-string-III.cpp

--- week2/day4/reverse-word-in-a-string-III.cpp
+++ week2/day4/reverse-word-in-a-string-III.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
 
+    string reverseWords(string s) {
+      string res;
+      for(int i = 0; i<s.size(); i++) {
+        int j = i;
+        while(j<s.size() && s[j] != ' ') {
+            j++;
+        }
+        int idx = i;
+       
+        string x = "";
+        for (int k = j - 1; k >= idx; k--) {
+                x += s[k];
+            }
+        res += x;
+        if(j<s.size()) {
+            res += ' ';
+        }
+        i = j;
+      } 
+      return res;
+    }
+};
