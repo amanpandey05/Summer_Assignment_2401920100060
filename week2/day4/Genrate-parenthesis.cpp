bb11db7123d0fb636e3867a1c590c4b3eea3180b
amanpandey05 <7266064524aman@gmail.com>
2026-06-11 22:16:14 +0530

    Update Genrate-parenthesis.cpp

--- week2/day4/Genrate-parenthesis.cpp
+++ week2/day4/Genrate-parenthesis.cpp
@@ -1,1 +1,39 @@
+class Solution {
+public:
+ vector<string> res;
+bool isvalid(string &s) {
+    stack<char> st;
+    for(int i = 0; i<s.size(); i++) {
+    if(st.empty()) {
+        st.push(s[i]);
+    } else {
+        if(s[i] == ')' && st.top() == '(') {
+            st.pop();
+        } else {
+            st.push(s[i]);
+        }
+    }
+    }
+    if(st.empty()) return true;
+    return false;
+} 
+void paren(string s, int n) {
+   
+     if(s.size() == 2*n) {
+        if(isvalid(s)) res.push_back(s);
+        return;
+    }
+    s.push_back('(');
+    paren(s,n);
+    s.pop_back();
 
+    s.push_back(')');
+    paren(s,n);
+    s.pop_back();
+}
+    vector<string> generateParenthesis(int n) {
+        string s = "";
+        paren(s,n);
+        return res;
+    }
+};
