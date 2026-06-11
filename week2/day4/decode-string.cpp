febccb073cc26885fa2e55733aad48acc0b06c62
amanpandey05 <7266064524aman@gmail.com>
2026-06-11 23:42:42 +0530

    Update decode-string.cpp

--- week2/day4/decode-string.cpp
+++ week2/day4/decode-string.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+public:
+    string decodeString(string s) {
+       stack<string> ss;
+       stack<int> cs;
+        int k = 0;
+        string curr = "";
+        for(auto c: s) {
+            if(isdigit(c)){
+                k = k*10+(c-'0');
+                
+            } 
+            else if(c == '[') {
+                cs.push(k);
+                ss.push(curr);
+                k = 0;
+                curr = "";
+            }
+            else if(c == ']') {
+                int cnt = cs.top();
+                cs.pop();
+                string prevstr = ss.top();
+                ss.pop();
+                string big = "";
+                for(int i = 0; i<cnt; i++) {
+                    big += curr;
+                }
+            curr = prevstr + big;
+            }
+            else {
+                curr += c;
+            }
+        }
+        return curr;
+    }
+};
