34b1478af11b0faf76dfdccf5b693408d99f2b17
amanpandey05 <7266064524aman@gmail.com>
2026-06-15 21:22:05 +0530

    Create llinked-list-cyclle.cpp

---
+++ week-3/day-1/llinked-list-cyclle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        if(head == NULL || head->next == NULL) return false;
+        
+
+        ListNode* temp1 = head;
+        ListNode* temp2 = head;
+        while(temp2 != NULL && temp2->next != NULL){
+            temp1 = temp1->next;
+            temp2 = temp2->next->next;
+           if(temp1 == temp2){
+            return true;
+           }
+        }
+        return false;
+    }
+};
